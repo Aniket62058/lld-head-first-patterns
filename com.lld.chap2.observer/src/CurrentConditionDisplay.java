8276babbf65375e840b81c0c39b8e8fc6d16fe53
Aniket Shandilya <aniket62058@gmail.com>
2026-06-19 11:32:04 +0530

    feat: Implemented Observer pattern for weather data display

---
+++ com.lld.chap2.observer/src/CurrentConditionDisplay.java
@@ -0,0 +1,22 @@
+public class CurrentConditionDisplay implements Observer, DisplayElements{
+    private float temperature;
+    private float humidity;
+    private WeatherData weatherData;
+
+    public CurrentConditionDisplay(WeatherData weatherData) {
+        this.weatherData = weatherData;
+        weatherData.addObserver(this);
+    }
+
+    @Override
+    public void update() {
+        this.temperature = weatherData.getTemperature();
+        this.humidity = weatherData.getHumidity();
+        display();
+    }
+
+    @Override
+    public void display() {
+        System.out.println("Current conditions: " + temperature + "F degrees and " + humidity + "%  humidity");
+    }
+}
